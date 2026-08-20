13557ab636e904d1a392a4733d7d1374d45501cd
Mugara250 <mbonyumugara52@gmail.com>
2026-08-20 14:09:24 +0200

    pizza razor page

--- RazorPagesTutorial/Pages/Pizza.cshtml.cs
+++ RazorPagesTutorial/Pages/Pizza.cshtml.cs
@@ -4,8 +4,26 @@ namespace RazorPagesTutorial.Pages;
 
 public class Pizza : PageModel
 {
+    public double Total { get; set; }
+    public string Customer { get; set; }
+    public string Order { get; set; }
+    public bool ExtraCheese { get; set; }
+
+    public double PizzaTotal(string pizzaType)
+    {
+        Dictionary<string, double> PizzaCost = new Dictionary<string, double>()
+        {
+            { "Cheese", 10 },
+            { "Pepperoni", 11 },
+            { "Vegetarian", 12 },
+        };
+        return PizzaCost[pizzaType];
+    }
     public void OnGet()
     {
-        
+        Customer = "Mushi Mbonyumugara";
+        Order = "Cheese";
+        ExtraCheese = false;
+        Total = PizzaTotal("Cheese");
     }
 }
