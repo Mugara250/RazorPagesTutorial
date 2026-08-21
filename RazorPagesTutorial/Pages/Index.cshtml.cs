3dc47cf7fc724339bd1e8540d411eed685a975ca
Mugara250 <mbonyumugara52@gmail.com>
2026-08-21 14:55:50 +0200

    OnGet() and OnPost() methods

--- RazorPagesTutorial/Pages/Index.cshtml.cs
+++ RazorPagesTutorial/Pages/Index.cshtml.cs
@@ -5,7 +5,14 @@ namespace RazorPagesTutorial.Pages;
 
 public class IndexModel : PageModel
 {
+    public string Title { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public string Body { get; set; } = null!;
     public void OnGet()
     {
+        Title = "Cuban Midnight Sandwich";
+        Date = new DateTime(2001, 1, 21);
+        Body =
+            "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
     }
 }
