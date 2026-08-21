75d067fc75710c1b44cba9e3cdd30b6ce5e2ed94
Mugara250 <mbonyumugara52@gmail.com>
2026-08-21 15:40:43 +0200

    model binding

--- RazorPagesTutorial/Pages/Index.cshtml.cs
+++ RazorPagesTutorial/Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ namespace RazorPagesTutorial.Pages;
 
 public class IndexModel : PageModel
 {
+    public string PostTitle { get; set; } = null!;
+    public DateTime PostDate { get; set; }
+    public string PostBody { get; set; } = null!;
     [BindProperty]
     public string Title { get; set; } = null!;
     [BindProperty]
@@ -13,9 +16,9 @@ public class IndexModel : PageModel
     public string Body { get; set; } = null!;
     public void OnGet()
     {
-        Title = "Cuban Midnight Sandwich";
-        Date = new DateTime(2001, 1, 21);
-        Body =
+        PostTitle = "Cuban Midnight Sandwich";
+        PostDate = new DateTime(2001, 1, 21);
+        PostBody =
             "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
         
     }
