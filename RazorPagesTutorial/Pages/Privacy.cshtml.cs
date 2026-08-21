3a460a0ebd3c309f9702e72387b3325e543f6af5
Mugara250 <mbonyumugara52@gmail.com>
2026-08-21 12:07:51 +0200

    tag helpers

--- RazorPagesTutorial/Pages/Privacy.cshtml.cs
+++ RazorPagesTutorial/Pages/Privacy.cshtml.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPagesTutorial.Pages;
 
 public class PrivacyModel : PageModel
 {
+    public string Country { get; set; } = null!;
+    public List<SelectListItem> Countries { get; set; } = null!;
+
     public void OnGet()
     {
+        Countries =
+        [
+            new SelectListItem("RW", "Rwanda"),
+            new SelectListItem("UG", "Uganda"),
+            new SelectListItem("SA", "South Africa"),
+            new SelectListItem("KE", "Kenya"),
+            new SelectListItem("TZ", "Tanzania")
+        ];
     }
 }
