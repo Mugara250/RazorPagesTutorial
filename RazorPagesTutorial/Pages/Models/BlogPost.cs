1af91e614ef4b6023357bf16e524e29a7f9ad4d8
Mugara250 <mbonyumugara52@gmail.com>
2026-08-21 19:14:37 +0200

    asp-page and asp-route-index

---
+++ RazorPagesTutorial/Pages/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+namespace RazorPagesTutorial.Pages.Models;
+
+public class BlogPost
+{
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Body { get; set; }
+
+    public BlogPost(string title, DateTime date, string body)
+    {
+        Title = title;
+        Date = date;
+        Body = body;
+    }
+    
+}
--- RazorPagesTutorial/Pages/Pizza.cshtml.cs
+++ RazorPagesTutorial/Pages/Pizza.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Pages.Models;
 
 namespace RazorPagesTutorial.Pages;
 
@@ -8,7 +9,16 @@ public class Pizza : PageModel
     public string Customer { get; set; } = "";
     public string Order { get; set; } = "";
     public bool ExtraCheese { get; set; }
-
+    
+    public readonly List<BlogPost> Archive = new List<BlogPost>
+    {
+        new BlogPost("One Pot Thai-Style Rice", new DateTime(2020, 1, 24), "This is a famous dish popularized in Thailand. Although the recipe varies from cook to cook and region to region, this is a good attempt at recreating what I ate from Thai-owned hole-in-the-wall restaurants in Okinawa, Japan. Key to the flavor are the sugar levels, unsalted peanuts, peanut oil, and either oyster or fish sauce."),
+        new BlogPost("Balsamic Glazed Salmon Fillets", new DateTime(2020, 1, 20), "A glaze featuring balsamic vinegar, garlic, honey, white wine and Dijon mustard makes baked salmon fillets extraordinary."),
+        new BlogPost("Spicy Garlic Lime Chicken", new DateTime(2019, 9, 4), "A delightful chicken dish with a little spicy kick. Serve with rice and your favorite vegetable."),
+        new BlogPost("Mushroom Pork Chops", new DateTime(2019, 6, 14), "Quick and easy, but very delicious. One of my family's favorites served over brown rice."),
+    };
+    
+    public BlogPost Displayed { get; set; }
     public double PizzaTotal(string pizzaType)
     {
         Dictionary<string, double> pizzaCost = new Dictionary<string, double>()
@@ -19,11 +29,22 @@ public class Pizza : PageModel
         };
         return pizzaCost[pizzaType];
     }
-    public void OnGet()
+    public void OnGet(int index)
     {
         Customer = "Mushi Mbonyumugara";
         Order = "Cheese";
         ExtraCheese = false;
         Total = PizzaTotal("Cheese");
+
+        Displayed = Archive[0];
+
+        if (index >= Archive.Count || index < 0)
+        {
+            Displayed = new BlogPost("n/a", new DateTime(0001, 01, 01), "n/a");
+        }
+        else
+        {
+            Displayed = Archive[index];
+        }
     }
 }
