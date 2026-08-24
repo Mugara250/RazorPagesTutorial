0a3c730fafd0dfb67046b35b891fa97d859209b0
Mugara250 <mbonyumugara52@gmail.com>
2026-08-24 10:42:50 +0200

    route template

--- RazorPagesTutorial/Pages/Index.cshtml.cs
+++ RazorPagesTutorial/Pages/Index.cshtml.cs
@@ -5,20 +5,7 @@ namespace RazorPagesTutorial.Pages;
 
 public class IndexModel : PageModel
 {
-    [BindProperty]
-    public string Title { get; set; } = null!;
-    [BindProperty]
-    public DateTime Date { get; set; }
-    [BindProperty]
-    public string Body { get; set; } = null!;
-    public void OnGet()
-    {
-        Title = "Cuban Midnight Sandwich";
-        Date = new DateTime(2001, 1, 21);
-        Body =
-            "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
-        
-    }
+    public void OnGet() {}
 
     public void OnPost() {}
 }
--- RazorPagesTutorial/Pages/Privacy.cshtml.cs
+++ RazorPagesTutorial/Pages/Privacy.cshtml.cs
@@ -8,6 +8,14 @@ public class PrivacyModel : PageModel
 {
     public string Country { get; set; } = null!;
     public List<SelectListItem> Countries { get; set; } = null!;
+    [BindProperty] public string Title { get; set; } = "";
+    [BindProperty] public DateTime Date { get; set; } = DateTime.Today;
+
+    [BindProperty] public string Body { get; set; } = "";
+
+    public string RecentPostTitle { get; set; } = "";
+    public DateTime RecentPostDate { get; set; }
+    public string RecentPostBody { get; set; } = "";
 
     public void OnGet()
     {
@@ -19,5 +27,15 @@ public class PrivacyModel : PageModel
             new SelectListItem("KE", "Kenya"),
             new SelectListItem("TZ", "Tanzania")
         ];
+        RecentPostTitle = "Cuban Midnight Sandwich";
+        RecentPostDate = new DateTime(2001, 1, 21);
+        RecentPostBody = "This sandwich is called a 'Media Noche' which translates to 'Midnight.' It makes a wonderful dinner sandwich because it is served hot. A nice side dish is black bean soup or black beans and rice, and plaintain chips.";
+    }
+
+    public void OnPost()
+    {
+        RecentPostTitle = Title;
+        RecentPostDate = Date;
+        RecentPostBody = Body;
     }
 }
