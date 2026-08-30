364c6ecae79697430b541e6e1e0ef13a9367c1ff
Mugara250 <mbonyumugara52@gmail.com>
2026-08-30 23:17:16 +0200

    form input validation

--- BlazorServerTutorial/Models/Client.cs
+++ BlazorServerTutorial/Models/Client.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorServerTutorial.Models;
 
 public class Client
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
     public string Name { get; set; } = "";
+    [Required]
+    [EmailAddress(ErrorMessage = "You must provide a valid email address")]
     public string Email { get; set; } = "";
+    [Required]
+    [StringLength(50, ErrorMessage = "Company name must not exceed 50 characters")]
     public string Company { get; set; } = "";
     public bool IsActive { get; set; }
     
