e182e3f4aca1ea60931dc11e7cfe26633770e614
Mugara250 <mbonyumugara52@gmail.com>
2026-09-03 16:58:48 +0200

    add home route

--- MyApiApp/Program.cs
+++ MyApiApp/Program.cs
@@ -19,6 +19,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => "Weather Forecast API");
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
